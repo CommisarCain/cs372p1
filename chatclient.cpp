22a03b6c9c9506f0e25b83b35b514926e9100307
pi <pi.dev>
2018-02-05 16:13:49 -0500

    all functional

--- chatclient.cpp
+++ chatclient.cpp
@@ -24,6 +24,31 @@ void error (const char* msg){
 	exit(0);
 }
 
+// this function is meant to handle ensuring all of a message is sent
+int sendall(int client, char* msg, int* msg_len){
+	int total = 0;
+	int remain = *msg_len;
+	int n;
+		
+	while(total < *msg_len){
+		n = send(client, msg+total, remain, 0);
+		if(n == -1){
+			break;
+		}
+		total += n;
+		remain -= n;
+	}
+	*msg_len = total;
+	if(n== -1){
+		return -1;
+	}
+	else{
+		return 0;
+	}	
+	
+
+}
+
 int main(int argc, char* argv[]){
 	if(argc != 3){
 		cerr << "Usage: prog hostname port" << endl;
@@ -41,13 +66,14 @@ int main(int argc, char* argv[]){
 	string quit_c = "/quit";
 	string ts = "ts1 ";
 	string umsg;
+	string user_handle;
 	//client = socket(AF_INET, SOCK_STREAM, 0);
 	if (client < 0)
 		error("Error Opening Socket");
 	else
 		cout << "Socket created!" << endl;
 	
-	//clean struct mem
+	//clean struct me
 	bzero((char*) &serv_addr, sizeof(serv_addr));
 		
 	//sets the socket addr info in the 
@@ -61,15 +87,17 @@ int main(int argc, char* argv[]){
 	if(getaddrinfo(argv[1], argv[2], &hints, &res) != 0)
 		cout << "ERROR GETTING ADDRESS" << endl;
 	
-	printf("Arg addr:%s:%s\n",argv[1],argv[2]);//debug check addresses 
+	//printf("Arg addr:%s:%s\n",argv[1],argv[2]);//debug check addresses 
+
 	/*Holy Smokes. So apparently when you want to get an addr
 	setup it returns a linked list, so you need to iterate over it and figure out
 	which struct is a valid one. I think in a prac app you would need to hand
 	shake with the server better to avoid MiM attacks. You also need to check 
 	if the socket you aquired is a valid socket before checking if it will yield 
 	a connection. After that you need to check if your selected socket is real or if 
-	iterated out of you list and have nothing instead.
+	iterated out of your list and have nothing instead.
 	*/	
+
 	for(p = res; p!= NULL; p = p->ai_next){
 		if((client = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
 			perror("client : socket failed to aquire\n");
@@ -88,28 +116,38 @@ int main(int argc, char* argv[]){
 		return 2;
 	}
 	
-	cout << "Connection to the server with port: " << to_string(s_port) << endl;
+	//cout << "Connection to the server with port: " << to_string(s_port) << endl;
 	numb = recv(client, rmsg, MSG_SIZE-1, 0);
 	rmsg[numb] = '\0'; //you recv bytes. Gotta put the null ending in.
-	cout << "Connection confirmed" << endl; 	
+	//cout << "Connection confirmed" << endl; 	
 	cout << rmsg << endl;	
+	umsg.resize(501);
+	cout << "Enter nickname" << endl;
+	getline(cin, user_handle);
+	user_handle.append(1,'>');
 	do{	
-		cout << "Input message:" << endl;
-		//cin >> smsg;
+		
+		cout << user_handle ;
 		getline(cin, umsg);
-		cout << "sending" << endl;
-		int result = send(client, umsg.c_str(), sizeof(umsg.c_str()), 0);
-		// need to check that all bytes are sent. Currently about 4 bytes
-		// are making it out in a transmission
-		printf("Amount of bytes sent: %d \n", result);
-		cout << "sent" << endl;
 		if(umsg.compare(quit_c) == 0){
 			//close(client);
 			cout << "quitting" << endl;
 			run_state = false;
 			break;
 		}
-		cout << "recieving" << endl;
+ 		umsg.insert(0,user_handle);
+ 
+ 		//cout << "input is: " << umsg << endl;
+ 		//loop here to ensure total message length was sent
+		int* m_size; 
+		*m_size = umsg.size();
+		char* out_msg = new char[umsg.length()+1];
+		strcpy(out_msg, umsg.c_str()); 
+		//cout << "out_msg: " << out_msg << endl;
+		int result = sendall(client, out_msg, m_size);
+		delete[] out_msg;
+		//cout << "sent" << endl;
+		//cout << "recieving: ";
 		numb = recv(client, rmsg, MSG_SIZE, 0);
 		rmsg[numb] = '\0';
 		cout << rmsg << endl;
@@ -117,7 +155,6 @@ int main(int argc, char* argv[]){
 
 	}while(run_state == true);
 	
-	cout << "END OF FILE" << endl;	
 	
 	
 }
--- chatserve.py
+++ chatserve.py
@@ -19,28 +19,62 @@ class Chat_Server:
 		self.clients = []
 		self.client_addresses = []
 		self.BUFFER_SZ = 500
-		print("Server started on port %s" % port)
+		self.name = S.gethostname()
+		self.threads = []
+		self.running = True
+		print("Server started on port %s" % port)	
 		print("Host name: %s" %S.gethostname())
 
+	def chatter(self):
+		conn = Thread(target=self.handle_New_Conn)
+		self.threads.append(conn)
+		conn.start()
+		while True:
+			msg = input(self.name + ">")
+			if msg == "/quit":
+				self.quitAll()
+				break
+			self.broadcast((self.name + ">" + msg).encode(), None)
+			
+
 	def handle_New_Conn(self):
-		while True:#spawns a new thread for every connection after handling
+		self.ssoc.settimeout(0.5)
+		while self.running:#spawns a new thread for every connection after handling
 			   #the initial setup
-			print("Accepting Connections!!!")
-			client, client_address = self.ssoc.accept()#handle client connecting
-			print("{} has connected".format(client_address))
-			self.clients.append(client)#list of all clients for repeating msg
-			self.client_addresses.append(client_address) #collect ips 
-			client.send("connected!".encode())#all transmissions have 
+			#print("Accepting Connections!!!")
+			try:	
+				client, client_address = self.ssoc.accept()#handle client connecting
+			except:
+				client = None
+			if client:	
+				#print("{} has connected".format(client_address))
+				self.clients.append(client)#list of all clients for repeating msg
+				self.client_addresses.append(client_address) #collect ips 
+				client.send("connected!".encode())#all transmissions have 
 							  #to be in bytes
-			nt = self.client_Thread #get function alias
-			Thread(target=nt, args=(client,)).start()
+				nt = self.client_Thread #get function alias
+				conn = Thread(target=nt, args=(client,))
+				self.threads.append(conn)
+				conn.start()
 
 	def client_Thread(self, client):
-		while True:
-			msg = client.recv(self.BUFFER_SZ)#if you recieve 0 bytes
-							 #then client dc'ed
+		while self.running:
+			try:
+				msg = client.recv(self.BUFFER_SZ)#if you recieve 0 bytes
+								 #then client dc'ed
+			except:
+				try:
+					client.close()
+					self.clients.remove(client)
+					break
+				except ValueError:
+					pass
+
 			if msg:
-				print("broadcast called")
+				print(" ")
+				print(msg.decode())
+				print(self.name +">", end="", flush=True)
+				
 				self.broadcast(msg, client)
 			else:
 				client.close()
@@ -52,13 +86,16 @@ class Chat_Server:
 				break
 
 	def broadcast(self, msg, client):
-		print(msg)
+		#print(msg)
 		for sock in self.clients:
-			sock.send(msg)#  inc message is byte encoded no need to encode
+			if sock != client:
+				sock.send(msg)#  inc message is byte encoded no need to encode
 
+	def quitAll(self):
+		self.running = False
 
 cs = Chat_Server(sys.argv[1], 5)
-top_thread = Thread(target=cs.handle_New_Conn)
+top_thread = Thread(target=cs.chatter)
 top_thread.start()
 top_thread.join()
 
